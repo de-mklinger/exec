d6fe12d681a7eb184003cf22a2b58481b04eec57
Marc Klinger <mklinger@mklinger.de>
2013-12-02 17:11:13 +0100

    Log message on error

--- src/main/java/de/mklinger/commons/exec/PipeRunnable.java
+++ src/main/java/de/mklinger/commons/exec/PipeRunnable.java
@@ -12,45 +12,47 @@ import org.slf4j.LoggerFactory;
  * @author Marc Klinger - mklinger[at]mklinger[dot]de
  */
 public class PipeRunnable implements Runnable {
-	private static final Logger LOG = LoggerFactory.getLogger(PipeRunnable.class);
-	private final OutputStream out;
-	private final InputStream in;
-	private Throwable error;
-	private final AtomicBoolean running = new AtomicBoolean();
+    private static final Logger LOG = LoggerFactory.getLogger(PipeRunnable.class);
+    private final OutputStream out;
+    private final InputStream in;
+    private Throwable error;
+    private final AtomicBoolean running = new AtomicBoolean();
 
-	public PipeRunnable(final InputStream in, final OutputStream out) {
-		this.in = in;
-		this.out = out;
-	}
+    public PipeRunnable(final InputStream in, final OutputStream out) {
+        this.in = in;
+        this.out = out;
+    }
 
-	@Override
-	public void run() {
-		running.set(true);
-		try {
-			final int copied = IOUtils.copy(in, out);
-			LOG.debug("Copied {} bytes", copied);
-		} catch (final Throwable e) {
-			this.error = e;
-		} finally {
-			running.set(false);
-		}
-	}
+    @Override
+    public void run() {
+        running.set(true);
+        try {
+            final int copied = IOUtils.copy(in, out);
+            LOG.debug("Copied {} bytes", copied);
+        } catch (final Throwable e) {
+            // TODO interrupt main thread here if we are in waitFor()? Or read and discard contents
+            LOG.error("Error in PipeRunnable", e);
+            this.error = e;
+        } finally {
+            running.set(false);
+        }
+    }
 
-	public Throwable getError() {
-		return error;
-	}
+    public Throwable getError() {
+        return error;
+    }
 
-	public boolean isRunning() {
-		return running.get();
-	}
+    public boolean isRunning() {
+        return running.get();
+    }
 
-	public void waitFor(final long timeoutMillis) throws CommandLineException, InterruptedException {
-		final long start = System.currentTimeMillis();
-		while (isRunning()) {
-			if (start + timeoutMillis < System.currentTimeMillis()) {
-				throw new CommandLineException("Timout waiting for pipe");
-			}
-			Thread.sleep(1);
-		}
-	}
+    public void waitFor(final long timeoutMillis) throws CommandLineException, InterruptedException {
+        final long start = System.currentTimeMillis();
+        while (isRunning()) {
+            if (start + timeoutMillis < System.currentTimeMillis()) {
+                throw new CommandLineException("Timout waiting for pipe");
+            }
+            Thread.sleep(1);
+        }
+    }
 }
