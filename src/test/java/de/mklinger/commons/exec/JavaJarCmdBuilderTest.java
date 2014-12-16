373d66a08291e0e549d13b293900f6e7727a00bb
Marc Klinger <mklinger@mklinger.de>
2014-12-16 20:25:33 +0100

    Updated license header

--- src/test/java/de/mklinger/commons/exec/JavaJarCmdBuilderTest.java
+++ src/test/java/de/mklinger/commons/exec/JavaJarCmdBuilderTest.java
@@ -1,4 +1,5 @@
-/* Copyright 2013, 2014 mklinger GmbH
+/*
+ * Copyright 2013-present mklinger GmbH - http://www.mklinger.de
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
